6355f2df37b82bf7c7b1b11d1f7443ee8882950a
Tiger Shen <tigershen23@gmail.com>
2023-02-06 16:21:16 +0000

    Added Zendesk. Gonna tinker with combining stuff now

--- qa/backup_welcome_zendesk.py
+++ qa/backup_welcome_zendesk.py
@@ -9,10 +9,10 @@ import requests
 credentials = "tiger@experiencewelcome.com", os.environ.get("ZENDESK_PASSWORD")
 print(credentials)
 zendesk = 'https://experiencewelcome.zendesk.com'
-language = 'en'
+language = 'en-us'
 
 date = datetime.date.today()
-backup_path = os.path.join(str(date), language)
+backup_path = os.path.join("data", "welcome_zendesk", str(date))
 if not os.path.exists(backup_path):
     os.makedirs(backup_path)
 
@@ -20,7 +20,33 @@ log = []
 
 endpoint = zendesk + '/api/v2/help_center/{locale}/articles.json'.format(locale=language.lower())
 
-response = requests.get(endpoint, auth=credentials)
+while endpoint:
+    response = requests.get(endpoint, auth=credentials)
+    if response.status_code != 200:
+        print('Failed to retrieve articles with error {}'.format(response.status_code))
+        exit()
 
-print(response.status_code)
-print(response.json())
+    data = response.json()
+
+    for article in data['articles']:
+        if article['body'] is None:
+            continue
+        title = '<h1>' + article['title'] + '</h1>'
+        filename = '{id}.html'.format(id=article['id'])
+        with open(os.path.join(backup_path, filename), mode='w', encoding='utf-8') as f:
+            f.write(title + '\n' + article['body'])
+        print('{id} copied!'.format(id=article['id']))
+
+        log.append((filename, article['title'], article['author_id']))
+
+    endpoint = data['next_page']
+
+with open(os.path.join(backup_path, '_log.csv'), mode='wt', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow( ('File', 'Title', 'Author ID') )
+    for article in log:
+        writer.writerow(article)
+
+# print(response.status_code)
+# print(response.json())
+# curl -u tiger@experiencewelcome.com:CU3QpXjdVPAjqv7 https://experiencewelcome.zendesk.com/api/v2/users/me.json
--- qa/streamlit_app.py
+++ qa/streamlit_app.py
@@ -1,7 +1,8 @@
+# cSpell:disable
 # For now, it's a manual process of updating this file when we make changes to document_qa.ipynb
 # MAKE SURE YOU'VE RUN THE CRAWLERS BEFORE RUNNING THIS APP OR IT WON'T WORK
 
-from gpt_index import GPTListIndex, SimpleWebPageReader, SimpleDirectoryReader, BeautifulSoupWebReader, GPTSimpleVectorIndex
+from gpt_index import GPTTreeIndex, SimpleWebPageReader, SimpleDirectoryReader, BeautifulSoupWebReader, GPTSimpleVectorIndex
 from langchain.agents import load_tools, Tool, initialize_agent, ZeroShotAgent, AgentExecutor
 from langchain.llms import OpenAI
 from langchain import OpenAI, LLMChain
@@ -17,6 +18,7 @@ path = os.path.dirname(__file__)
 def get_marketing_site_index():
     welcome_marketing_site_data = SimpleDirectoryReader(path +"/crawls/collections/welcome_marketing/pages").load_data()
     welcome_marketing_site_index = GPTSimpleVectorIndex(welcome_marketing_site_data)
+    welcome_marketing_site_index.set_text("Welcome Marketing Site")
 
     return welcome_marketing_site_index
 
@@ -44,6 +46,7 @@ def get_marketing_site_agent():
 def get_blog_index():
     welcome_blog_data = SimpleDirectoryReader(path + "/crawls/collections/welcome_marketing/pages").load_data()
     welcome_blog_index = GPTSimpleVectorIndex(welcome_blog_data)
+    welcome_blog_index.set_text("Welcome Blog")
 
     return welcome_blog_index
 
@@ -65,13 +68,80 @@ def get_blog_agent():
     return agent
 #endregion
 
+#region Zendesk supporting code
+# Set up document QA index
+@st.cache(allow_output_mutation=True)
+def get_zendesk_index():
+    welcome_zendesk_data = SimpleDirectoryReader(path + "/data/welcome_zendesk/2023-02-06").load_data()
+    welcome_zendesk_index = GPTSimpleVectorIndex(welcome_zendesk_data)
+    welcome_zendesk_index.set_text("Welcome Zendesk")
+
+    return welcome_zendesk_index
+
+# Query DB
+def query_zendesk_db(query: str):
+    return get_zendesk_index().query(query, verbose=True)
+
+# Create LangChain agent
+def get_zendesk_agent():
+    tools = [
+        Tool(
+            name="QueryingDB",
+            func=query_zendesk_db,
+            description="Returns most relevant answer from document for query string",
+        )
+    ]
+    llm = OpenAI(temperature=0.1)
+    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+    return agent
+#endregion
+
+#region combined supporting code
+# Set up document QA index
+@st.cache(allow_output_mutation=True)
+def get_combined_index():
+    welcome_marketing_site_index = get_marketing_site_index()
+    welcome_blog_index = get_blog_index()
+    welcome_zendesk_index = get_zendesk_index()
+
+    return GPTTreeIndex([welcome_marketing_site_index, welcome_blog_index, welcome_zendesk_index])
+
+# Query DB
+def query_combined_db(query: str):
+    return get_combined_index().query(query, verbose=True)
+
+# Create LangChain agent
+def get_combined_agent():
+    tools = [
+        Tool(
+            name="QueryingDB",
+            func=query_combined_db,
+            description="Returns most relevant answer from document for query string",
+        )
+    ]
+    llm = OpenAI(temperature=0.1)
+    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+    return agent
+#endregion
+
+def hide_streamlit_img_full_screen():
+    hide_img_full_screen = '''
+    <style>
+    button[title="View fullscreen"]{
+        visibility: hidden;}
+    </style>
+    '''
+
+    return st.markdown(hide_img_full_screen, unsafe_allow_html=True)
+
 # Streamlit interface
 def main():
     # st.title("Welcome Question-Answering")
     welcome_logo = Image.open(path + "/static/welcome_light.png")
     st.image(welcome_logo, width=200)
-    st.header("Welcome Marketing Site Q&A")
+    hide_streamlit_img_full_screen()
 
+    st.header("Welcome Marketing Site Q&A")
     marketing_site_search = st.container()
     marketing_site_query = marketing_site_search.text_input(
         'Ask a question about anything public about Welcome!',
@@ -90,7 +160,6 @@ def main():
     st.write("━" * 34)
 
     st.header("Welcome Blog Q&A")
-
     blog_search = st.container()
     blog_query = blog_search.text_input(
         "Ask a question about anything written on Welcome's blog!",
@@ -105,6 +174,42 @@ def main():
             blog_agent = get_blog_agent()
             blog_answer = blog_agent.run(blog_query)
         blog_search.write(blog_answer)
+
+    st.write("━" * 34)
+
+    st.header("Welcome ZenDesk Q&A")
+    zendesk_search = st.container()
+    zendesk_query = zendesk_search.text_input(
+        "Ask a question about any of Welcome's support content!",
+        '',
+        placeholder='Ex: What are the recommended dimensions for event cover images on Welcome?'
+    )
+
+    previous_zendesk_query = ""
+    if zendesk_search.button("Go!", key="ZenDesk Go button") or zendesk_query != previous_zendesk_query:
+        previous_zendesk_query = zendesk_query
+        with st.spinner("Granting wishes..."):
+            zendesk_agent = get_zendesk_agent()
+            zendesk_answer = zendesk_agent.run(zendesk_query)
+        zendesk_search.write(zendesk_answer)
+
+    st.write("━" * 34)
+
+    st.header("Welcome Q&A")
+    combined_search = st.container()
+    combined_query = combined_search.text_input(
+        "Ask about Welcome!",
+        '',
+        placeholder='Ex: What 3rd-party platforms does Welcome integrate with?'
+    )
+
+    previous_combined_query = ""
+    if combined_search.button("Go!", key="Combined Go button") or combined_query != previous_combined_query:
+        previous_combined_query = combined_query
+        with st.spinner("Spinning the hamster..."):
+            combined_agent = get_combined_agent()
+            combined_answer = combined_agent.run(combined_query)
+        combined_search.write(combined_answer)
 
 if __name__ == "__main__":
     main()
